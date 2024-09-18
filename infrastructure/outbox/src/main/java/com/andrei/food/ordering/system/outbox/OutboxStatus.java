3c352fcdc74c4e2e7fd4f72bf025f922f1eec460
Andrei <andreibalzan@gmail.com>
2024-09-18 08:46:35 +0100

    updating order service database schema and config for outbox pattern

---
+++ infrastructure/outbox/src/main/java/com/andrei/food/ordering/system/outbox/OutboxStatus.java
@@ -0,0 +1,5 @@
+package com.andrei.food.ordering.system.outbox;
+
+public enum OutboxStatus {
+    STARTED, COMPLETED, FAILED
+}
---
+++ infrastructure/saga/src/main/java/com/andrei/food/ordering/system/SagaStatus.java
@@ -0,0 +1,5 @@
+package com.andrei.food.ordering.system;
+
+public enum SagaStatus {
+    STARTED, FAILED, SUCCEEDED, PROCESSING, COMPENSATING, COMPENSATED
+}
